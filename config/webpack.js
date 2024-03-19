48643b197a7e9ea618eb1b596fbc7b19b7a85d4d
PSPDFKit <support@pspdfkit.com>
2024-03-19 16:47:07 +0100

    PDF viewer web application built with PSPDFKit for Web and Elm

---
+++ config/webpack.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "../src/index.js"),
+  mode: "development",
+  resolve: {
+    extensions: [".js", ".elm"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: {
+          loader: "elm-webpack-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "./node_modules/pspdfkit/dist/pspdfkit-lib",
+          to: "./pspdfkit-lib",
+        },
+        {
+          from: "./assets/example.pdf",
+          to: "./example.pdf",
+        },
+      ],
+    }),
+  ],
+};
